e661c69f726f01c1eda8f85369cbda04606a667c
Hashrocket Workstation <dev@hashrocket.com>
2014-05-10 09:38:51 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lukeransom::Application.config.secret_key_base = '3cbcf72373bcbb101520c54e2e2927004cc56427bb4ea43c67987334ad7d420eb4da7a128d6a6f5e11b2393c5a4697471370a3aa15add3c16937a8fb59ff9882'
