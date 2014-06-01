53fb81b4a6a29940ec1e563d0d5f6b751db4a04d
Hashrocket Workstation <dev@hashrocket.com>
2014-06-01 16:09:23 -0400

    User edits projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,16 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update_attributes(project_params)
+    redirect_to projects_path
+  end
+
   def destroy
     Project.find(params[:id]).destroy
     redirect_to projects_path
